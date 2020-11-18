e988839274636a352a6ffdbc481e7dd8b429e102
Wander Mendes <wanderok@msn.com>
2020-11-18 09:48:01 -0300

    i7 para Geovana

--- EnviarParaGeovana.bat
+++ EnviarParaGeovana.bat
@@ -1,4 +1,5 @@
 C:
+del C:\wander\*.~* /s /y
 XCOPY C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\built\SMC_SISTEMA\SMCPLUS*.* C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\src\PROJETO
 xcopy C:\wander\Sistemas\Delphi\unisystem\SMC_PLUS\src C:\WANDER\OK\GEOVANA_OK\SRC /S /Y /I
 CD \WANDER\OK\GEOVANA_OK
